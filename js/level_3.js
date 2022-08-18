9088e29f265464a9ddc2276ba9d45e8229cf5357
dartsoma <109676829+dartsoma@users.noreply.github.com>
2022-08-18 14:33:49 -0400

    Version 0.3

--- js/level_3.js
+++ js/level_3.js
@@ -1,56 +1,61 @@
 
-let healthElement = document.getElementById("health")
+let healthElement = document.getElementById("health");
 let submitButton = document.getElementById("submitButton");
 const userInput = document.getElementById("answer");
 submitButton.addEventListener("click", form);
 
-num1 = randNumGenerator()
-num2 = randNumGenerator()
+
+
+num1 = randNumGenerator();
+num2 = randNumGenerator();
 let answer = num1 + num2;
 let score = 0;
-let health = 5;
+let health = 10;
 
 function form(event) {
     event.preventDefault();
-    if (userInput.value == ""){
+    if (userInput.value == "") {
         alert("Invalid Reponse")
     }
-    
+
     else {
-    if (userInput.value == answer) {    
-        console.log(answer)
-        score++;
-        document.getElementById("incre").innerHTML = "Score: "+ score;
-        
-    } else {
-        console.log("Wrong!");
-        console.log(answer)
-        health --;
-        healthElement.innerHTML = "Health: " + health;
-    }
-    }
-    if (score >= 10){
-        console.log("You win")
-        window.location.href = "/levels/you_win.html"
+        //submitButton.onclick = function(event) {
+            //event.preventDefault();
+            if (userInput.value == answer) {
+                console.log(answer);
+                score++;
+                 
+                document.getElementById("incre").innerHTML = "Score: " + score;
+            } else {
+                console.log("Wrong!");
+                console.log(answer);
+                health--;
+                healthElement.innerHTML = "Health: " + health;
+            // }
         }
-    if (health <= 0){
-            console.log("Lol u lost, get GUDDER")
-            window.location.href = "/levels/lose.html"
-            }
+    }
+    if (score >= 10) {
+        console.log("You win");
+        window.location.href = "../levels/level_2.html";
+    }
+    if (health <= 0) {
+        console.log("Lol u lost, how did you fail");
+        window.location.href = "../levels/lose.html";
+    }
     document.getElementById("answer").value = "";
-    
+
     document.getElementById("firstnum").innerHTML = num1 + " + " + num2 + " =";
-    answer = num1+num2
+    answer = num1 + num2;
     num1 = randNumGenerator();
-    
+
     num2 = randNumGenerator();
-    
+
 }
 
 
 function randNumGenerator() {
 
-    let randNumber = Math.random() * 101;
+    let randNumber = Math.random() * 100;
     // return 0 - (num1-1)
     let wholeNum = Math.floor(randNumber);
     return wholeNum;
@@ -63,5 +68,6 @@ num2 = randNumGenerator();
 document.getElementById("incre").innerHTML = "Score: " + score;
 document.getElementById("health").innerHTML = "Health: " + health;
 
-
-
+// function setup() {
+//     plsWork.play();
+// }
